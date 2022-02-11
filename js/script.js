14c7a2bd121564d933108e68cb35d42f4b8a07c5
jenifirfire <zenisbekkambaraliev19@gmail.com>
2022-02-11 08:21:06 +0600

    25_january

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// const creatOrderDiv = (name, amount) =>{
+
+//     const div = document.createElement("div");
+//     div.innerHTML = `${name} : <b> ${amount} </b>`;
+//     div.style.color = "purple";
+//     return div;
+// };
